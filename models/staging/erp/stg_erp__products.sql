cd3523a5aa35b771eaf982cc7a151ec22714d5f5
Gabriela Klitzke <g.amklitzke@gmail.com>
2024-08-08 01:36:39 +0000

    Fixing model issues

--- models/staging/erp/stg_erp__products.sql
+++ models/staging/erp/stg_erp__products.sql
@@ -4,16 +4,19 @@ with
             cast(PRODUCTID as int) as pk_product
             , cast(NAME as varchar) as product_name
             , cast(PRODUCTNUMBER as varchar) as product_number
-            , cast(PRODUCTLINE as varchar) as product_line
             , cast(CLASS as varchar) as product_class
             , cast(STYLE as varchar) as product_style
             , cast(PRODUCTSUBCATEGORYID as int) as fk_product_subcategory
             , cast(MODIFIEDDATE as datetime) as dt_modified
             , case
-                when DISCONTINUEDDATE is null then false
-                when DISCONTINUEDDATE is not null then true
+                when DISCONTINUEDDATE is null or SELLENDDATE is null then false
+                when DISCONTINUEDDATE is not null or SELLENDDATE is not null then true
                 else null
             end as is_discontinued
+            , case
+                when PRODUCTLINE::varchar is null then 'O' -- Indicate "Other", products that are not bicycles.
+                else PRODUCTLINE
+            end as product_line
             --MAKEFLAG  -- this lines won't be used
             --FINISHEDGOODSFLAG
             --COLOR
@@ -31,6 +34,7 @@ with
             --SELLENDDATE
             -- ROWGUID
         from {{ source('erp', 'product') }}
+        where FINISHEDGOODSFLAG = 'True' --Selects only products that can be sold by Adventure Works.
     )
 select *
 from src_products
